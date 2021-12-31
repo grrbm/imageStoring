48942d8cc9c58ca0ef6e56caf7990bf3a02d013f
Guilherme Reis <grrbm2@gmail.com>
2021-12-31 20:11:34 -0300

    working initialization script

--- scripts/initContainers.sh
+++ scripts/initContainers.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
+docker container kill $(docker ps -q)
+docker system prune -f
+echo "done killing all running containers"
 docker build -t image-storing ../
-echo "done"
+echo "done building"
+docker run -d -e APP_PORT=8080 -e APP_HOST=localhost -e DAT_PORT=27017 -e DAT_HOST=localhost -p 8080:8080 image-storing
+echo "done executing"
