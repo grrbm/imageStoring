73d5374193c2bf074f31afcc8b88ab7a504c820d
Guilherme Reis <grrbm2@gmail.com>
2021-12-31 20:29:16 -0300

    finished script to start container and mongodb

--- scripts/initContainers.sh
+++ scripts/initContainers.sh
@@ -5,4 +5,6 @@ echo "done killing all running containers"
 docker build -t image-storing ../
 echo "done building"
 docker run -d -e APP_PORT=8080 -e APP_HOST=localhost -e DAT_PORT=27017 -e DAT_HOST=localhost -p 8080:8080 image-storing
-echo "done executing"
+echo "done executing image-storing service"
+docker run -d -p 2717:27017 -v data:/data/db -d -e MONGO_INITDB_ROOT_USERNAME=someuser -e MONGO_INITDB_ROOT_PASSWORD=pass123 --name mymongo mongo
+echo "done creating mongodb instance"
