7166538178fef6b7b2949e46dcd8894d97af949d
Guilherme Reis <grrbm2@gmail.com>
2022-01-01 20:50:33 -0300

    script demoC now reads all samples and post them to the database

--- scripts/demoC.sh
+++ scripts/demoC.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-for i in {1..10}
-do
-  echo "counter: $i"
-  MY_NAME=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c10)
-  curl -X POST -F caption=$MY_NAME -F file=@image1.jpg http://localhost:8080/api/create
-  echo "done uploading file $MY_NAME."
+for file in ../samples/*; 
+do 
+    echo "file: $file"; 
+    MY_NAME=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c10)
+    curl -X POST -F caption=$MY_NAME -F file=@$file http://localhost:8080/api/create
+    echo -e "\ndone uploading file $MY_NAME."
 done
-# curl -X POST -v -F caption=valueKK -F file=@image1.jpg http://localhost:8080/api/create
