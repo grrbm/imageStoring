2bf4f0c18b0e365c88dd380512b170a8d7d0b658
Guilherme Reis <grrbm2@gmail.com>
2022-01-02 14:36:16 -0300

    create and delete working

--- scripts/demoC.sh
+++ scripts/demoC.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
+i=0
 for file in ../samples/*; 
 do 
+    ((++i))
     echo "file: $file"; 
-    MY_NAME=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c10)
-    curl -X POST -F caption=$MY_NAME -F file=@$file http://localhost:8080/api/create
+    curl -X POST -F caption="image$i" -F file=@$file http://localhost:8080/api/create
     echo -e "\ndone uploading file $MY_NAME."
 done
---
+++ scripts/demoD.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+i=0
+for file in ../samples/*; 
+do 
+    ((++i))
+    echo "file: $file"; 
+    curl -X DELETE -d caption="image$i" http://localhost:8080/api/delete
+    echo -e "\ndone deleting file $file."
+done
