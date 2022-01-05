be282b8146286d73c8aa39b08595e8385a849ebf
Guilherme Reis <grrbm2@gmail.com>
2022-01-05 15:14:33 -0300

    switched port to 27018

--- scripts/initContainersSeparately.sh
+++ scripts/initContainersSeparately.sh
@@ -5,10 +5,10 @@ docker volume rm data #run this command if you want to completely restart the da
 echo "done killing all running containers"
 docker network create mern-net
 echo "created network mern-net"
-docker run --name mongodb -v /"$(pwd)"/mongo-init.js:/docker-entrypoint-initdb.d/mongo-init.js -v data:/data/db --rm -d -p 27017:27017 --network mern-net mongo --bind_ip 0.0.0.0 --port 27017
+docker run --name mongodb -v /"$(pwd)"/mongo-init.js:/docker-entrypoint-initdb.d/mongo-init.js -v data:/data/db --rm -d -p 27018:27017 --network mern-net mongo --bind_ip 0.0.0.0 --port 27018
 echo "done creating mongodb instance"
 docker build -t image-storing ../
 echo "done building"
 #docker run -d -e APP_PORT=8080 -e APP_HOST=localhost -e DAT_PORT=27017 -e DAT_HOST=localhost -p 8080:8080 --network mern-net image-storing
-docker run -d -e APP_PORT=8080 -e APP_HOST=0.0.0.0 -e DAT_PORT=27017 -e DAT_HOST=mongodb -e MONGODB_USERNAME=myUserAdmin -e MONGODB_PASSWORD=pass -d --rm -p 8080:8080 --network mern-net image-storing
+docker run -d -e APP_PORT=8080 -e APP_HOST=0.0.0.0 -e DAT_PORT=27018 -e DAT_HOST=mongodb -e MONGODB_USERNAME=myUserAdmin -e MONGODB_PASSWORD=pass -d --rm -p 8080:8080 --network mern-net image-storing
 echo "done executing image-storing service"
